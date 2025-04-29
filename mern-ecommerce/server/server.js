4aca812e0f1fc17840b89346fced8cf721e037c4
Chundru Sohan Kumar <sohankumarchundru@gmail.com>
2025-04-29 23:10:00 +0530

    updated in .env

--- mern-ecommerce/server/server.js
+++ mern-ecommerce/server/server.js
@@ -22,11 +22,11 @@ mongoose
   .catch((error) => console.log(error));
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT =process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.CLIENT_BASE_URL,
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
